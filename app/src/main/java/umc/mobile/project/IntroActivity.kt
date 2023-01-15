4ce8058e3f397d66c9f538d2c5eab9f46e2f9af5
jjjuyoa <jjjuyoa@gmail.com>
2023-01-15 21:44:50 +0900

    fix: intro 화면 추가 및 인트로 화면을 첫번째 activity로 설정

---
+++ app/src/main/java/umc/mobile/project/IntroActivity.kt
@@ -0,0 +1,24 @@
+package umc.mobile.project
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import umc.mobile.project.databinding.ActivityIntroBinding
+
+
+class IntroActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityIntroBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewBinding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, 3000)
+
+    }
+}

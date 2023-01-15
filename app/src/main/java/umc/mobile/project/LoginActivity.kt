a72289acf2377f8b1cafa4f6c486095e1e9c6143
jjjuyoa <jjjuyoa@gmail.com>
2023-01-15 20:21:11 +0900

    feat: 로그인 성공 다이얼로그 구현

--- app/src/main/java/umc/mobile/project/LoginActivity.kt
+++ app/src/main/java/umc/mobile/project/LoginActivity.kt
@@ -1,14 +1,26 @@
 package umc.mobile.project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import umc.mobile.project.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), MyCustomDialogInterface {
     private lateinit var viewBinding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
+
+        viewBinding.btnLogin.setOnClickListener {
+            val myCustomDialog = MyCustomDialog(this, this)
+            myCustomDialog.show()
+        }
     }
+
+    override fun onbtnGotoMainClicked() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
 }

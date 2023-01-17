bdb080c5ec18138ad0f1b9b8fa9c9a48f2adb5da
hini5971 <80618658+hini5971@users.noreply.github.com>
2023-01-18 01:18:16 +0900

    test

--- app/src/main/java/umc/mobile/project/HomeFragment.kt
+++ app/src/main/java/umc/mobile/project/HomeFragment.kt
@@ -1,18 +1,29 @@
 package umc.mobile.project
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import umc.mobile.project.databinding.FragmentHomeBinding
+import umc.mobile.project.databinding.FragmentRestaurantBinding
+import umc.mobile.project.notice.NoticeData
+import umc.mobile.project.notice.NoticeListActivity
+import umc.mobile.project.notice.NoticeRVAdapter
 
 class HomeFragment: Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View {
-        return FragmentHomeBinding.inflate(layoutInflater).root
+        return FragmentRestaurantBinding.inflate(layoutInflater).root
+        }
+
+
     }
-}
+
+
+

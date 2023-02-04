d9c3b5929141e5ae5943de7e67a17185ffb27666
jjjuyoa <jjjuyoa@gmail.com>
2023-02-04 15:54:19 +0900

    feat: 이메일로 비밀번호 찾기 api 연결

--- app/src/main/java/umc/mobile/project/login/ApiLoginService.kt
+++ app/src/main/java/umc/mobile/project/login/ApiLoginService.kt
@@ -2,7 +2,9 @@ package umc.mobile.project.login
 
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface ApiLoginService {
 
@@ -10,5 +12,10 @@ interface ApiLoginService {
     fun userLogin(
         @Body user : LoginRequest
     ): Call<LoginResponse>
+
+    @GET("user/findPwd")
+    fun findPwd(
+        @Query ("email") email : String
+    ): Call<FindPwdResponse>
 
 }

eb9a793c1becc44fcd0352128b2c65af2d1eac8e
yeon015 <lizkitty@naver.com>
2023-03-31 23:22:28 +0900

    feat: 최신순 좋아요 조회 및 추가 #yeoni

--- app/src/main/java/umc/mobile/project/DataRecentRVAdapter.kt
+++ app/src/main/java/umc/mobile/project/DataRecentRVAdapter.kt
@@ -3,11 +3,8 @@ package umc.mobile.project
 import android.content.Context
 import Post
 import android.util.Log
-import android.util.SparseBooleanArray
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.BaseAdapter
-import android.widget.Button
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -16,7 +13,6 @@ import umc.mobile.project.announcement.AnnounceAlertDialogInterface
 import umc.mobile.project.announcement.Auth.LikePost.LikePostResult
 import umc.mobile.project.announcement.Auth.LikePost.LikePostService
 import umc.mobile.project.announcement.Auth.LikePost.Result
-import umc.mobile.project.announcement.Auth.PostPost.userPostId
 import umc.mobile.project.databinding.ItemDataBinding
 import umc.mobile.project.ram.Geocoder_location
 import umc.mobile.project.ram.my_application_1.post_id_to_detail
@@ -24,17 +20,26 @@ import umc.mobile.project.ram.my_application_1.user_id_logined
 import umc.mobile.project.ram.my_application_1.user_id_var
 import umc.mobile.project.wishlist.GetLikePost.LikePostGetResult
 import umc.mobile.project.wishlist.GetLikePost.LikePostGetService
+import umc.mobile.project.wishlist.Like
+import umc.mobile.project.wishlist.LikePost
 
-class DataRecentRVAdapter(private val homeDataList: ArrayList<Post>) : RecyclerView.Adapter<DataRecentRVAdapter.RecentViewHolder>() {
+class DataRecentRVAdapter(private val homeDataList: ArrayList<Post>) : RecyclerView.Adapter<DataRecentRVAdapter.RecentViewHolder>(), LikePostGetResult {
 
     lateinit var context : Context
 
     private var contentPostId = 0
 
+    private val clickedLikeStatus = arrayListOf<Post>()
+
+    private lateinit var likes : Like
+
+    private var itemPosition : Int = 0
+
+
     //ViewHolder 객체
     inner class RecentViewHolder(val viewBinding: ItemDataBinding) :
-        RecyclerView.ViewHolder(viewBinding.root) , LikePostResult, AnnounceAlertDialogInterface,
-        LikePostGetResult {
+        RecyclerView.ViewHolder(viewBinding.root) , LikePostResult, AnnounceAlertDialogInterface
+         {
 
         fun bind(homeData: Post) {
 
@@ -57,39 +62,33 @@ class DataRecentRVAdapter(private val homeDataList: ArrayList<Post>) : RecyclerV
             viewBinding.annRecruit.text = homeData.num_of_recruits.toString() //총 인원
 
             contentPostId = homeData.post_id
-            getLikePost()
+
+            Log.d("absoluteAdapterPosition: ", absoluteAdapterPosition.toString())
+
+            viewBinding.btnLikeThird.setBackgroundResource(R.drawable.main_item_heart_icon)
+
+            var id = clickedLikeStatus.find { it.post_id == homeData.post_id }
+            if(id != null) {
+                Log.d("체크된 post 제목 ======================== ", homeData.title)
+                viewBinding.btnLikeThird.setBackgroundResource(R.drawable.main_item_heart_icon_fill)
+            }
+
+
 
             viewBinding.btnLikeThird.setOnClickListener {
                 val likePostService = LikePostService()
                 likePostService.setLikePostResult(this)
                 likePostService.sendLike(user_id_logined, homeData.post_id)
                 Log.d("post_id: ", homeData.post_id.toString())
+
             }
 
         }
 
-        private fun getLikePost() {
-            val likePostGetService = LikePostGetService()
-            likePostGetService.setLikePostGetResult(this)
-            likePostGetService.getLikePost(user_id_logined)
-        }
 
-        override fun getPostUploadSuccess(code: Int, result: ArrayList<Post>) {
-            for(i in 0 .. result.size - 1) {
-                if(contentPostId == result[i].post_id) {
-                    Log.d("contentPostId: ", contentPostId.toString())
-                    Log.d("result.post_id", result[i].post_id.toString())
-                    viewBinding.btnLikeThird.setBackgroundResource(R.drawable.main_item_heart_icon_fill)
-                    break;
-                }
-            }
-        }
-
-        override fun getPostUploadFailure(code: Int, message: String) {
-            Log.d("", "")
-        }
 
         override fun LikePostSuccess(result: Result) {
+//            likes.isSelected = viewBinding.btnLikeThird.isSelected
             viewBinding.btnLikeThird.setBackgroundResource(R.drawable.main_item_heart_icon_fill)
             notifyDataSetChanged()
             Log.d("관심목록 등록 성공", "")
@@ -117,37 +116,59 @@ class DataRecentRVAdapter(private val homeDataList: ArrayList<Post>) : RecyclerV
 
     //ViewHolder 만들어질 때 실행할 동작
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): DataRecentRVAdapter.RecentViewHolder {
-
         val viewBinding: ItemDataBinding = ItemDataBinding.inflate(
             LayoutInflater.from(viewGroup.context),
             viewGroup, false)
         context = viewGroup.context
+        getLikePost()
+
         return RecentViewHolder(viewBinding)
 
     }
 
-    //ViewHolder가 실제로 데이터를 표시해야 할 때 호출되는 함수
-    override fun onBindViewHolder(holder: DataRecentRVAdapter.RecentViewHolder, position: Int) {
-
-        holder.bind(homeDataList[position])
-        post_id_to_detail = homeDataList[position].post_id
-        holder.itemView.setOnClickListener {
-            user_id_var = homeDataList[position].user_id
-            Log.d("user_id : ===========" , user_id_var.toString())
-            itemClickListener.onItemClick(homeDataList[position])
-            notifyItemChanged(position)
-        }
-
-
+    private fun getLikePost() {
+        val likePostGetService = LikePostGetService()
+        likePostGetService.setLikePostGetResult(this)
+        likePostGetService.getLikePost(user_id_logined)
     }
 
+    var success_isCliced = false
 
-    override fun getItemViewType(position: Int): Int {
-        return position
+    override fun getPostUploadSuccess(code: Int, result: ArrayList<Post>) {
+        clickedLikeStatus.addAll(result)
+        for(i in 0..result.size-1)
+            Log.d("title : ", result[i].title)
+    }
+
+    override fun getPostUploadFailure(code: Int, message: String) {
+        Log.d("", "")
+    }
+
+    //ViewHolder가 실제로 데이터를 표시해야 할 때 호출되는 함수
+    override fun onBindViewHolder(holder: DataRecentRVAdapter.RecentViewHolder, position: Int) {
+        if(holder is RecentViewHolder) {
+//                holder.bind(homeDataList[position], clickedLikeStatus[position])
+            holder.bind(homeDataList[position])
+            post_id_to_detail = homeDataList[position].post_id
+
+
+//            var id = clickedLikeStatus.find { it.post_id == homeDataList[position].post_id }
+//            if(id != null) {
+//                Log.d("체크된 post 제목 ======================== ", homeDataList[position].title)
+//                holder.viewBinding.btnLikeThird.setBackgroundResource(R.drawable.main_item_heart_icon_fill)
+//            }
+
+            holder.itemView.setOnClickListener {
+                user_id_var = homeDataList[position].user_id
+                Log.d("user_id : ===========", user_id_var.toString())
+                itemClickListener.onItemClick(homeDataList[position])
+                notifyItemChanged(position)
+            }
+        }
     }
 
     //표현할 아이템의 총 갯수
-    override fun getItemCount(): Int = homeDataList.size
+    override fun getItemCount(): Int = if (homeDataList.isNullOrEmpty()) 0 else homeDataList.size
 
 
     //2
@@ -164,3 +185,189 @@ class DataRecentRVAdapter(private val homeDataList: ArrayList<Post>) : RecyclerV
 
 
 }
+
+
+//package umc.mobile.project
+//
+//import android.content.Context
+//import Post
+//import android.util.Log
+//import android.view.LayoutInflater
+//import android.view.ViewGroup
+//import android.widget.Toast
+//import androidx.recyclerview.widget.RecyclerView
+//import com.bumptech.glide.Glide
+//import umc.mobile.project.announcement.AnnounceAlertDialog
+//import umc.mobile.project.announcement.AnnounceAlertDialogInterface
+//import umc.mobile.project.announcement.Auth.LikePost.LikePostResult
+//import umc.mobile.project.announcement.Auth.LikePost.LikePostService
+//import umc.mobile.project.announcement.Auth.LikePost.Result
+//import umc.mobile.project.databinding.ItemDataBinding
+//import umc.mobile.project.ram.Geocoder_location
+//import umc.mobile.project.ram.my_application_1.post_id_to_detail
+//import umc.mobile.project.ram.my_application_1.user_id_logined
+//import umc.mobile.project.ram.my_application_1.user_id_var
+//import umc.mobile.project.wishlist.GetLikePost.LikePostGetResult
+//import umc.mobile.project.wishlist.GetLikePost.LikePostGetService
+//import umc.mobile.project.wishlist.Like
+//
+//class DataRecentRVAdapter(private val homeDataList: ArrayList<Post>) : RecyclerView.Adapter<DataRecentRVAdapter.RecentViewHolder>() {
+//
+//    lateinit var context : Context
+//
+//    private var contentPostId = 0
+//
+//    private val clickedLikeStatus = arrayListOf<Like>()
+//
+//    private lateinit var likes : Like
+//
+//    private var itemPosition : Int = 0
+//
+//    //ViewHolder 객체
+//    inner class RecentViewHolder(val viewBinding: ItemDataBinding) :
+//        RecyclerView.ViewHolder(viewBinding.root) , LikePostResult, AnnounceAlertDialogInterface,
+//        LikePostGetResult {
+//
+//        fun bind(homeData: Post, clickStatus: Like) {
+//
+//            Glide.with(context).load(homeData.image).centerCrop().into(viewBinding.ivItemImageThird)
+//
+//            viewBinding.tvItemTitle.text = homeData.title //제목
+//
+//            var latLong_to_address : String = Geocoder_location().calculate_location(context, homeData.latitude, homeData.longitude)
+//            var txt_location = latLong_to_address
+//            viewBinding.tvItemWhere.text = txt_location // 주소
+//
+//            val txt_time = homeData.order_time
+////            2022-01-23T03:34:56.000+00:00
+//            var txt_hour = txt_time.substring(11 until 13)
+//            var txt_minute = txt_time.substring(14 until 16)
+//            var txt_time_substring = txt_hour+"시" + txt_minute + "분 주문"
+//            viewBinding.tvItemTime.text = txt_time_substring // 주문한 시간
+//
+//            viewBinding.annApp.text = homeData.recruited_num.toString() // 신청인원
+//            viewBinding.annRecruit.text = homeData.num_of_recruits.toString() //총 인원
+//
+//            contentPostId = homeData.post_id
+//
+//            likes = clickStatus
+//
+//            viewBinding.btnLikeThird.isSelected = clickStatus.isSelected
+//
+//            itemPosition = absoluteAdapterPosition
+//
+//            Log.d("absoluteAdapterPosition: ", absoluteAdapterPosition.toString())
+//
+//            getLikePost()
+//
+//            viewBinding.btnLikeThird.setOnClickListener {
+//
+//
+//                val likePostService = LikePostService()
+//                likePostService.setLikePostResult(this)
+//                likePostService.sendLike(user_id_logined, homeData.post_id)
+//                Log.d("post_id: ", homeData.post_id.toString())
+//            }
+//
+//        }
+//
+//        private fun getLikePost() {
+//            val likePostGetService = LikePostGetService()
+//            likePostGetService.setLikePostGetResult(this)
+//            likePostGetService.getLikePost(user_id_logined)
+//        }
+//
+//        override fun getPostUploadSuccess(code: Int, result: ArrayList<Post>) {
+//            for(i in 0 .. result.size - 1) {
+//                if(contentPostId == result[i].post_id) {
+//                    Log.d("contentPostId: ", contentPostId.toString())
+//                    Log.d("result.post_id", result[i].post_id.toString())
+//                    viewBinding.btnLikeThird.setBackgroundResource(R.drawable.main_item_heart_icon_fill)
+//
+//                    likes.isSelected = viewBinding.btnLikeThird.isSelected
+//
+//                    clickedLikeStatus.add(Like(itemPosition, true))
+//
+//                    notifyItemChanged(adapterPosition)
+//
+//                    break;
+//                }
+//            }
+//        }
+//
+//        override fun getPostUploadFailure(code: Int, message: String) {
+//            Log.d("", "")
+//        }
+//
+//        override fun LikePostSuccess(result: Result) {
+//            likes.isSelected = viewBinding.btnLikeThird.isSelected
+//            viewBinding.btnLikeThird.setBackgroundResource(R.drawable.main_item_heart_icon_fill)
+//            notifyDataSetChanged()
+//            Log.d("관심목록 등록 성공", "")
+//            Log.d("post_id: ", result.postIdx.toString())
+//            Log.d("likeIdx: ", result.likeIdx.toString())
+//            Log.d("post_User_userIdx: ", result.post_User_userIdx.toString())
+//            Log.d("like_userIdx: ", result.like_userIdx.toString())
+//            Toast.makeText(context, "관심 공고 등록 성공.", Toast.LENGTH_SHORT).show()
+//        }
+//
+//        override fun LikePostFailureMyPost() {
+//            val dlg = AnnounceAlertDialog(context, this)
+//            dlg.start5()
+//        }
+//
+//        override fun LikePostFailureAdd() {
+//            val dlg = AnnounceAlertDialog(context, this)
+//            dlg.start6()
+//        }
+//
+//        override fun btnFinish() {
+//        }
+//
+//    }
+//
+//    //ViewHolder 만들어질 때 실행할 동작
+//    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): DataRecentRVAdapter.RecentViewHolder {
+//
+//        val viewBinding: ItemDataBinding = ItemDataBinding.inflate(
+//            LayoutInflater.from(viewGroup.context),
+//            viewGroup, false)
+//        context = viewGroup.context
+//        return RecentViewHolder(viewBinding)
+//
+//    }
+//
+//    //ViewHolder가 실제로 데이터를 표시해야 할 때 호출되는 함수
+//    override fun onBindViewHolder(holder: DataRecentRVAdapter.RecentViewHolder, position: Int) {
+//            if(holder is RecentViewHolder) {
+////                holder.bind(homeDataList[position], clickedLikeStatus[position])
+//                holder.bind(homeDataList[position], clickedLikeStatus[position])
+//                post_id_to_detail = homeDataList[position].post_id
+//                holder.itemView.setOnClickListener {
+//                    user_id_var = homeDataList[position].user_id
+//                    Log.d("user_id : ===========", user_id_var.toString())
+//                    itemClickListener.onItemClick(homeDataList[position])
+//                    notifyItemChanged(position)
+//                }
+//
+//            }
+//    }
+//
+//    //표현할 아이템의 총 갯수
+//    override fun getItemCount(): Int = if (homeDataList.isNullOrEmpty()) 0 else homeDataList.size
+//
+//
+//    //2
+//    interface OnItemClickListener {
+//        fun onItemClick(homeData: Post)
+//
+//    }
+//    fun setItemClickListener(onItemClickListener: OnItemClickListener) {
+//        this.itemClickListener = onItemClickListener
+//    }
+//
+//    private lateinit var itemClickListener : OnItemClickListener
+//
+//
+//
+//}
---
+++ app/src/main/java/umc/mobile/project/wishlist/Like.kt
@@ -0,0 +1,3 @@
+package umc.mobile.project.wishlist
+
+data class Like (val position:Int, var isSelected:Boolean)
